4dfbb48fddcb694c694f873b5460f0bb39cdb4c5
eric <ericlin66.cs11@nycu.edu.tw>
2023-08-17 03:37:06 +0800

    Implement q_new() and q_free()

--- queue.c
+++ queue.c
@@ -14,11 +14,22 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *l = malloc(sizeof(*l));
+    if (l)
+        INIT_LIST_HEAD(l);
+    return l;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    if (!l)
+        return;
+    element_t *node, *safe;
+    list_for_each_entry_safe (node, safe, l, list)
+        q_release_element(node);
+    free(l);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
