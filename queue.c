28d1d63d4d4f353839460cf3b639c44de5e1a26b
eric <ericlin66.cs11@nycu.edu.tw>
2023-08-17 16:05:20 +0800

    Implement q_remove_head() and q_remove_tail()

--- queue.c
+++ queue.c
@@ -59,13 +59,28 @@ bool q_insert_tail(struct list_head *head, char *s)
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    // cppcheck-suppress nullPointer
+    element_t *ele = list_first_entry(head, element_t, list);
+    if (!ele)
+        return NULL;
+    if (sp && bufsize) {
+        size_t from_len = strlen(ele->value);
+        size_t to_len = (from_len > bufsize - 1) ? bufsize - 1 : from_len;
+        memcpy(sp, ele->value, to_len);
+        sp[to_len] = '\0';
+    }
+    list_del(&ele->list);
+    return ele;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    return q_remove_head(head->prev->prev, sp, bufsize);
 }
 
 /* Return number of elements in queue */
